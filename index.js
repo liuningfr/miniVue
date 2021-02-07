41b850cb8b1d489a6231a0ad42bee4614609d36f
liuning <liuning@liuning.local>
2021-02-07 16:49:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+class miniVue {
+  constructor(options) {
+    this.$options = options;
+		this.$data = options.data;
+    this.$el = options.el;
+
+    this.observer(this.$data);
+  }
+
+  // Observer
+  observer(data) {
+    if(!data || typeof(data) !== 'object'){
+			return;
+    }
+
+    Object.keys(data).forEach(key => {
+      this.observerSet(key, data, data[key]);
+      // 把 data 下的数据挂载到 this 下
+			this.proxyData(key);
+		});
+  }
+
+  observerSet(key, data, value) {
+    Object.defineProperty(data, key, {
+      get() {
+        return value;
+      },
+      set(newValue) {
+        if (newValue === value) {
+				  return;
+        }
+        value = newValue;
+      },
+    });
+  }
+
+  proxyData(key) {
+    Object.defineProperty(this, key, {
+      get() {
+        return this.$data[key];
+      },
+      set(newValue){
+        this.$data[key] = newValue;
+      }
+		});
+  }
+}
